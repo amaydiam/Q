dd46863e9478b9a55039f9a00fdf1410270d443a
binderbyte <binderbyte@gmail.com>
2017-01-11 13:52:19 +0700

    add validator login and register

--- app/src/main/java/com/ad/sample/ui/activity/RegisterUserActivity.java
+++ app/src/main/java/com/ad/sample/ui/activity/RegisterUserActivity.java
@@ -1,9 +1,12 @@
 package com.ad.sample.ui.activity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ad.sample.R;
@@ -19,6 +22,14 @@ import com.joanzapata.iconify.fonts.FontAwesomeModule;
 import com.joanzapata.iconify.fonts.MaterialCommunityModule;
 import com.joanzapata.iconify.fonts.MaterialModule;
 import com.joanzapata.iconify.fonts.SimpleLineIconsModule;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Length;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,19 +39,29 @@ public class RegisterUserActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_login)
     RobotoRegularTextView btnLogin;
-
-    @BindView(R.id.nama)
-    RobotoRegularEditText nama;
-    @BindView(R.id.email)
-    RobotoRegularEditText email;
-    @BindView(R.id.password)
-    RobotoRegularEditText password;
     @BindView(R.id.btn_register)
     RobotoRegularButton btnRegister;
     @BindView(R.id.btn_facebook)
     FloatingActionButton btnFacebook;
     @BindView(R.id.btn_google_plus)
     FloatingActionButton btnGooglePlus;
+
+    @NotEmpty
+    @Length(min = 2, max = 50, trim = true, messageResId = R.string.val_name_length)
+    @BindView(R.id.nama)
+    RobotoRegularEditText nama;
+
+    @NotEmpty
+    @Length(min = 6, max = 50, trim = true, messageResId = R.string.val_email_length)
+    @Email
+    @BindView(R.id.email)
+    RobotoRegularEditText email;
+
+    @NotEmpty
+    @Length(min = 4, max = 100, trim = true, messageResId = R.string.val_password_length)
+    @Password
+    @BindView(R.id.password)
+    RobotoRegularEditText password;
 
     @OnClick(R.id.btn_login)
     void ActionLogin(){
@@ -50,7 +71,8 @@ public class RegisterUserActivity extends AppCompatActivity {
     @OnClick(R.id.btn_register)
     void SubmitRegister(){
         //Login
-        //  startActivity(new Intent(this,LoginActivity.class));
+        validator.validate();
+        //startActivity(new Intent(this,LoginActivity.class));
     }
 
     @OnClick(R.id.btn_facebook)
@@ -63,6 +85,8 @@ public class RegisterUserActivity extends AppCompatActivity {
         Toast.makeText(this, "Login Via Google plus", Toast.LENGTH_SHORT).show();
     }
 
+    private Validator validator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Iconify
@@ -74,6 +98,7 @@ public class RegisterUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_user);
         ButterKnife.bind(this);
+        setValidator();
         //set underline
         btnLogin.setPaintFlags(btnLogin.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
         //set icon
@@ -85,6 +110,44 @@ public class RegisterUserActivity extends AppCompatActivity {
                 new IconDrawable(this, EntypoIcons.entypo_google)
                         .colorRes(R.color.blue_2196F3)
                         .actionBarSize());
+    }
+
+    private void setValidator() {
+        validator = new Validator(this);
+        validator.setValidationListener(new Validator.ValidationListener() {
+            @Override
+            public void onValidationSucceeded() {
+                registerSucces();
+            }
+
+            @Override
+            public void onValidationFailed(List<ValidationError> errors) {
+                registerFailed(errors);
+            }
+        });
+    }
+
+    private void registerSucces() {
+
+        String result_nama = nama.getText().toString();
+        String result_email = email.getText().toString();
+        String result_password = password.getText().toString();
+
+        startActivity(new Intent(this, LoginUserActivity.class));
+    }
+
+    private void registerFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(getApplicationContext());
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
